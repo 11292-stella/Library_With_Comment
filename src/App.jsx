adb29dff6f44a148eda0784d79119a0b53b1fcef
11292-stella <stella.marucelli@gmail.com>
2025-04-08 13:46:27 +0200

    modifica delle classi

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,32 @@ import { Container, Row, Col } from "react-bootstrap"
 import horrorB from "./horror.json"
 import DinuovoBook from "./components/DinuovoBook"
 import NewComment from "./components/NewComment"
-import { Component } from "react"
+import { useState } from "react"
 
-class App extends Component {
-  state = {
-    selectedAsin: null,
-  }
+const App = function () {
+  //state = {
+  //selectedAsin: null,
+  //}
 
-  handleBookSelect = (asin) => {
+  const [selectedAsin, setSelectedAsin] = useState("")
+
+  const handleBookSelect = (asin) => {
     console.log("ASIN selezionato:", asin)
-    this.setState({ selectedAsin: asin })
+    setSelectedAsin(asin)
   }
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col lg={9} md={8} sm={12}>
-            <DinuovoBook libri={horrorB} onBookSelect={this.handleBookSelect} />
-          </Col>
-          <Col lg={3} md={4} sm={12} className="mt-3 mt-lg-0">
-            <NewComment asin={this.state.selectedAsin} />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg={9} md={8} sm={12}>
+          <DinuovoBook libri={horrorB} onBookSelect={handleBookSelect} />
+        </Col>
+        <Col lg={3} md={4} sm={12} className="mt-3 mt-lg-0">
+          <NewComment asin={selectedAsin} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default App
--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,37 +1,43 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 
 const URL = "https://striveschool-api.herokuapp.com/api/comments/"
 
-class NewComment extends Component {
-  state = {
-    comments: [],
-    newComment: "", // dove memorizziamo quello che scrive l’utente
-  }
+const NewComment = function (props) {
+  //state = {
+  // comments: [],
+  //newComment: "", // dove memorizziamo quello che scrive l’utente
+  //}
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.asin !== this.props.asin) {
-      console.log("ASIN cambiato:", this.props.asin)
-      this.fetchComments()
-    }
-  }
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState("")
 
-  fetchComments = () => {
-    if (!this.props.asin) return
-    fetch(URL + this.props.asin, {
+  //componentDidUpdate(prevProps) {
+  //if (prevProps.asin !== this.props.asin) {
+  //console.log("ASIN cambiato:", this.props.asin)
+  //this.fetchComments()
+  // }
+  // }//
+
+  useEffect(() => {
+    fetchComments()
+  }, [props.asin])
+
+  const fetchComments = () => {
+    if (!props.asin) return
+    fetch(URL + props.asin, {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2RkMjcwMzM4MzRiZjAwMTUwMDA3MDAiLCJpYXQiOjE3NDM2ODQ3MzYsImV4cCI6MTc0NDg5NDMzNn0.qS52rTci0AyAlbwFKzIjMXL4LY5O0JxHJizWLUJHWUM",
       },
     })
       .then((response) => response.json())
-      .then((data) => this.setState({ comments: data }))
+      .then((data) => setComments(data))
       .catch((error) => console.log("Errore nel fetch:", error))
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const { newComment } = this.state
-    const { asin } = this.props
+    const asin = props.asin
 
     if (!newComment || !asin) return
 
@@ -50,45 +56,50 @@ class NewComment extends Component {
     })
       .then((res) => res.json())
       .then(() => {
-        this.setState({ newComment: "" })
-        this.fetchComments()
+        fetchComments()
       })
       .catch((error) => console.log("Errore POST:", error))
   }
 
-  render() {
-    const { comments, newComment } = this.state
-    const { asin } = this.props
+  const asin = props.asin
 
-    return (
-      <div>
-        <h2>Commenti</h2>
-        {asin ? (
-          <>
-            <form onSubmit={this.handleSubmit}>
+  return (
+    <div>
+      <h2>Commenti</h2>
+      {asin ? (
+        <>
+          <form onSubmit={handleSubmit} className="mb-4">
+            <div className="form-group">
               <input
                 type="text"
+                className="form-control"
                 placeholder="Scrivi un commento..."
                 value={newComment}
-                onChange={(e) => this.setState({ newComment: e.target.value })}
+                onChange={(e) => setNewComment(e.target.value)}
               />
-              <button type="submit">Invia</button>
-            </form>
-            <h4>Commenti esistenti:</h4>
-            <ul>
-              {comments.length > 0 ? (
-                comments.map((c, i) => <li key={i}>{c.comment}</li>)
-              ) : (
-                <li>Nessun commento ancora.</li>
-              )}
-            </ul>
-          </>
-        ) : (
-          <p>Seleziona un libro per vedere i commenti</p>
-        )}
-      </div>
-    )
-  }
+            </div>
+            <button type="submit" className="btn btn-primary mt-2">
+              Invia
+            </button>
+          </form>
+          <h4>Commenti esistenti:</h4>
+          <ul className="list-group">
+            {comments.length > 0 ? (
+              comments.map((c, i) => (
+                <li key={i} className="list-group-item">
+                  {c.comment}
+                </li>
+              ))
+            ) : (
+              <li className="list-group-item">Nessun commento ancora.</li>
+            )}
+          </ul>
+        </>
+      ) : (
+        <p>Seleziona un libro per vedere i commenti</p>
+      )}
+    </div>
+  )
 }
 
 export default NewComment
